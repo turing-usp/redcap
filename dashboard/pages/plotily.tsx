3c209efad04dbcf39b9a8592d6d94f8b68cafac3
Guilherme Salustiano <guissalustiano@gmail.com>
2021-07-08 00:59:32 -0300

    feat: plotly react quebrado

--- dashboard/pages/plotily.tsx
+++ dashboard/pages/plotily.tsx
@@ -1,11 +1,35 @@
 import Layout from '../components/Layout'
 import { getAllredcap } from '../data-provider/redcap';
+// import Plot from 'react-plotly.js';
+import { Disease } from '../interfaces';
+import { count, objToDf } from '../util/agregator';
 
-const PlotilyPage = () => (
-  <Layout title="plotily">
-    <h1>plotily</h1>
-  </Layout>
-)
+type Props = {
+  data: Disease[]
+}
+
+const PlotilyPage = ({data}: Props) => {
+  const counter = data.map(d => d.ecog).filter(e => e != "").reduce(count, {})
+  const df = objToDf(counter)
+  return (
+    <Layout title="plotily">
+      <h1>Plotily</h1>
+      <div>Não consegui nem rodar o exemplo</div>
+      {/* <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: { color: 'red' },
+          },
+          { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+        ]}
+        layout={{ width: 320, height: 240, title: 'A Fancy Plot' }} /> */}
+    </Layout>
+  );
+}
 
 export async function getStaticProps() {
   const data = await getAllredcap();
